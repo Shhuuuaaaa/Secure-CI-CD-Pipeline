6ceeceaca1a289a7ee8f85018e5576d82ba9da51
JoshuaPr-dev <joshuaprv.contact@gmail.com>
2026-05-21 11:46:12 +0200

    test: modifier la clé AWS pour déclencher la détection de Gitleaks

--- app.py
+++ app.py
@@ -33,8 +33,8 @@ def calculator():
 
 # --- 3. Secret Hardcodé ( AWS Access Keys ) --- 
 # VULNERABILITE : Stocker des identifiants sensibles directement dans le code source
-AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
-AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7R89X2TB"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCY98ZTR45MLK"
 
 @app.route("/backups")
 def get_backups():
